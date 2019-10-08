9408c7c44a9cc9f3b33f5efefd41f0b12565da06
Chris Evans <chris.evans93@gmail.com>
2019-10-08 17:20:27 -0400

    Loop optimizations for bytecode

--- bfpyjit.py
+++ bfpyjit.py
@@ -33,6 +33,9 @@ OP_OPEN_JMP = 6
 OP_CLOSE_JMP = 7
 OP_MOVE = 8
 OP_CLEAR = 9
+OP_COPY = 10
+OP_SCANR = 11
+OP_SCANL = 12
 
 # Used to naively go from BF -> IR type
 instruction_opcode_map = {
@@ -57,7 +60,8 @@ opcode_name_map = {
     OP_OPEN_JMP: "openjmp",
     OP_CLOSE_JMP: "closejmp",
     OP_MOVE: "move",
-    OP_CLEAR: "clear"
+    OP_CLEAR: "clear",
+    OP_COPY: "copy"
 }
 
 
@@ -94,7 +98,7 @@ class Opcode(object):
 
     def __str__(self):
         name = opcode_to_string(self.op)
-        return f"{name} {self.offset} {self.arg if self.arg else ''}"
+        return f"{name} {self.offset} {self.arg}"
 
     def __repr__(self):
         name = opcode_to_string(self.op)
@@ -103,6 +107,99 @@ class Opcode(object):
 def cleanup(source):
     return "".join(filter(lambda x: x in instruction_opcode_map.keys(), source))
 
+def _is_clearloop(code, size, index, mptr):
+    """
+    Detects a clear loop and returns equivalent opcodes
+    """
+
+    if index < (size - 3):
+        clr = code[index : index + 3]
+        if clr == "[+]" or clr == "[-]":
+            return [Opcode(OP_CLEAR, mptr)], 3
+
+    return [], 0
+
+def _is_scanloop(code, size, index, mptr):
+    """
+    Detects a scan loop and returns equivalent opcodes
+    """
+
+    if index < (size - 3):
+        clr = code[index : index + 3]
+
+        if clr == "[>]":
+            return [Opcode(OP_SCANR, mptr)], 3
+
+        elif clr == "[<]":
+            return [Opcode(OP_SCANL, mptr)], 3
+
+    return [], 0
+
+def _is_copyloop(code, size, index, mptr):
+    # Copy/multiply loop must start with a decrement
+    if (index > (size - 6)) or (code[index + 1] != "-"):
+        return [], 0
+
+    mult = 0
+    depth = 0
+    mults = {}
+    i = index + 2
+
+    # Consume the loop contents until the cell pointer movement changes
+    # direction. Keep track of pointer movement, and the number of increments
+    # at each cell, so we can create Opcodes to recreate the copy / multiply
+    # operations performed by the loop
+    while i < size:
+        if code[i] in "><":
+            if mult > 0:
+                mults[depth] = mult
+                mult = 0
+
+            if code[i] == "<":
+                break
+
+            depth += 1
+
+        elif code[i] == "+":
+            mult += 1
+
+        else:
+            return [], 0
+
+        i += 1
+
+    # If no cell or pointer increments by now, this isn't a copy/multiply loop
+    if (len(mults) == 0) or (depth == 0) or (i == (size - 1)):
+        return [], 0
+
+    ret = [Opcode(OP_COPY, mptr, mults)]
+
+    # Consume all the pointer decrements until the end of the loop.
+    # If we encounter any non-"<" characters in the loop at this stage,
+    # this isn't a copy/multiply loop (at least, not one I want to mess with!)
+    while (i < size) and (code[i] != "]"):
+        if code[i] != "<":
+            return [], 0
+
+        depth -= 1
+        i += 1
+
+    if (depth != 0) or (i == (size - 1)):
+        return [], 0
+
+    return ret, (i - index) + 1
+
+def run_loop_optimizers(code, size, index, mptr):
+    loop_opts = [
+        _is_clearloop, _is_copyloop, _is_scanloop
+    ]
+
+    for opt in loop_opts:
+        optcode_list, n_instructions = opt(code, size, index, mptr)
+        if n_instructions > 0:
+            return optcode_list, n_instructions
+
+    return [], 0
 
 def parse(source):
     code = cleanup(source)
@@ -118,14 +215,12 @@ def parse(source):
 
         if opcode == OP_OPEN_JMP:
 
-            # Let's look for OP_CLEAR loops
-            if pc < (size - 3): # [+] or [-] need to be able to do +3
-                clear_instruction = code[pc:pc+3]
-                if clear_instruction in ["[+]", "[-]"]:
-                    opcodes.append(Opcode(OP_CLEAR, offset=mptr))
-                    pc += 3
-                    mptr = 0
-                    continue # continue since 
+            optcode_list, n_instructions = run_loop_optimizers(code, size, pc, mptr)
+            if n_instructions > 0:
+                opcodes.extend(optcode_list)
+                pc += n_instructions
+                mptr = 0
+                continue
 
 
             # If we are doing a loop to increment/decrement our memory ptr we
@@ -275,10 +370,27 @@ def evaluate(opcodes, data_input=None, buffer_output=True):
             if op.arg == 0:
                 memory[mptr] = 0
             # Coallesced clears
-            elif op.arg < 0:
-                memory[mptr] = (memory[mptr] - op.arg) % 256
+            elif op.arg < 0: memory[mptr] = (memory[mptr] - op.arg) % 256
             elif op.arg > 0:
                 memory[mptr] = (memory[mptr] + op.arg) % 256
+
+        elif op.op == OP_COPY:
+            mptr += op.offset
+            if memory[mptr] > 0:
+                for offset in op.arg:
+                    idx = mptr + offset
+                    memory[idx] = (memory[idx] + (memory[mptr] * op.arg[offset])) % 256
+                memory[mptr] = 0
+
+        elif op.op == OP_SCANR:
+            mptr += op.offset
+            while mptr > 0 and memory[mptr] != 0:
+                mptr -= 1
+
+        elif op.op == OP_SCANL:
+            mptr += op.offset
+            while mptr < (size - 1) and memory[mptr] != 0:
+                mptr += 1
 
         if(DEBUG):
             print(f"*op={op}\n* pc={pc}\n* dataptr={mptr}\n* Memory locations:")
@@ -295,8 +407,9 @@ def main():
     if len(sys.argv) == 2:
         with open(sys.argv[1], 'r') as f:
             opcodes = parse(f.read())
+            # for op in opcodes:
+            #     print(op)
             buffer = evaluate(opcodes, buffer_output=False)
-            print(buffer)
     else:
         print("Usage:", sys.argv[0], "filename")
 
